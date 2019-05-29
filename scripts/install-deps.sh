9fbe75e2ab8995b72231c453821c1e22bbf6e499
Kazuhiro Hayashi <zuka0828@gmail.com>
2019-05-29 19:29:25 +0000

    scripts: install-deps.sh: Update package list for warrior
    The following packages are added in the upstream document:
    8c7ec72e: python3-git python3-jinja2
    e63d516d: libegl1-mesa libsdl1.2-dev xterm
    
    Signed-off-by: Kazuhiro Hayashi <zuka0828@gmail.com>

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -9,4 +9,5 @@ fi
 
 apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib \
 build-essential chrpath socat cpio python python3 python3-pip python3-pexpect \
-xz-utils debianutils iputils-ping
+xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev \
+xterm
