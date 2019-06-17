de1c7d520298c162b4fa93b781682dbe9514f498
Trung Do <trung.dothanh@toshiba-tsdv.com>
2019-06-17 17:17:14 +0700

    tests: Rename test.sh to build_test.sh
    This script is only for build test.
    
    Signed-off-by: Trung Do <trung.dothanh@toshiba-tsdv.com>

--- tests/build_test.sh
+++ tests/build_test.sh
@@ -7,9 +7,15 @@ TEST_TARGETS=${TEST_TARGETS:-core-image-minimal}
 TEST_DISTROS=${TEST_DISTROS:-deby-tiny}
 TEST_MACHINES=${TEST_MACHINES:-qemux86}
 
+# Setup builddir
 cd $WORKDIR
 export TEMPLATECONF=meta-debian/conf
 source ./poky/oe-init-build-env
+
+# Get version of recipes
+all_versions=`pwd`/all_versions.txt
+bitbake -s > $all_versions
+
 for distro in $TEST_DISTROS; do
   sed -i -e "s/\(^DISTRO\s*?*=\).*/\1 \"$distro\"/" conf/local.conf
   for machine in $TEST_MACHINES; do
@@ -25,10 +31,11 @@ for distro in $TEST_DISTROS; do
     for target in $TEST_TARGETS; do
       bitbake $target 2>&1 | tee $LOGDIR/${target}.log
 
+      version=`grep "^$target\s*:" $all_versions | cut -d: -f2 | sed "s/ *$//"`
       if [ "$?" = "0" ]; then
-        echo "$target pass" >> $RESULT
+        echo "$target $version PASS" >> $RESULT
       else
-        echo "$target fail" >> $RESULT
+        echo "$target $version FAIL" >> $RESULT
       fi
     done
   done
