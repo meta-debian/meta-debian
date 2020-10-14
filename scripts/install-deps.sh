8221e61b3e506a09e2b3c95717ccf60304f9d1b9
Kazuhiro Hayashi <zuka0828@gmail.com>
2020-10-15 01:25:23 +0900

    scripts: install-deps.sh: apt install always says yes
    For scripts to automate setup processes like docker
    
    Signed-off-by: Kazuhiro Hayashi <zuka0828@gmail.com>

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -7,7 +7,7 @@ if [ "$(whoami)" != "root" ]; then
 	exit 1
 fi
 
-apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib \
+apt-get install -y gawk wget git-core diffstat unzip texinfo gcc-multilib \
 build-essential chrpath socat cpio python python3 python3-pip python3-pexpect \
 xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev \
 xterm
