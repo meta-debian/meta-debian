965284fba9edfa603ba54de066b6fcc07be341b7
Kazuhiro Hayashi <zuka0828@gmail.com>
2021-03-09 23:42:38 +0900

    scripts: install-deps.sh: Refine package list and apt-get options
    Split a monolithic package list into multiple lists that
    correspond to instructions in Yocto Project manual.
    
    Also, allow to pass options for apt-get install command from
    arguments instead of only passing -y.
    
    Signed-off-by: Kazuhiro Hayashi <zuka0828@gmail.com>

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
-# Install packages defined as the "Essentials" in
-# https://www.yoctoproject.org/docs/2.7/ref-manual/ref-manual.html#ubuntu-packages
+# Install packages required to use poky and meta-debian
 
 if [ "$(whoami)" != "root" ]; then
 	echo "Please run this script as root"
 	exit 1
 fi
 
-apt-get install -y gawk wget git-core diffstat unzip texinfo gcc-multilib \
-build-essential chrpath socat cpio python python3 python3-pip python3-pexpect \
-xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev \
-xterm
+# https://www.yoctoproject.org/docs/2.7/ref-manual/ref-manual.html#required-git-tar-and-python-versions
+PKG_ESSENTIALS="git tar python3"
+
+# "Essentials" in https://www.yoctoproject.org/docs/2.7/ref-manual/ref-manual.html#ubuntu-packages
+PKG_ESSENTIALS_DEBIAN=" \
+	gawk wget git-core diffstat unzip texinfo gcc-multilib \
+	build-essential chrpath socat cpio python python3 python3-pip \
+	python3-pexpect xz-utils debianutils iputils-ping python3-git \
+	python3-jinja2 libegl1-mesa libsdl1.2-dev xterm \
+	"
+
+# No extra package is required at the moment
+PKG_EXTRA=""
+
+apt-get install ${@} ${PKG_ESSENTIALS} ${PKG_ESSENTIALS_DEBIAN} ${PKG_EXTRA}
